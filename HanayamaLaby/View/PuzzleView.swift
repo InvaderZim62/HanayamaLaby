ea59682d28e0b49a3bea7a1564383986b98a8981
Invader Zim <pwdelete@gmail.com>
2022-01-17 11:43:31 -0800

    Draw spokes and arcs as shapes, instead of lines, since shapes have advantages in terms of creating holes and maybe determining if touch points are contained in them (and outside them).

--- HanayamaLaby/View/PuzzleView.swift
+++ HanayamaLaby/View/PuzzleView.swift
@@ -10,6 +10,7 @@ import UIKit
 struct PuzzleConst {
     static let wallWidth: CGFloat = 10
     static let wallColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+    static let floorColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
 }
 
 class PuzzleView: UIView {
@@ -18,57 +19,85 @@ class PuzzleView: UIView {
     var spokes = [Spoke]()
     var arcs = [Arc]()
     
-    lazy var puzzleCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+    private lazy var puzzleCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+//    private lazy var wholeView = UIBezierPath(rect: bounds)  // whole view to append holes in
+    
+    // rotate about point by translating (from 0,0) to point, rotating, and translating back
+    private func transformToRotate(angle: Double, about point: CGPoint) -> CGAffineTransform {
+        return CGAffineTransform(translationX: point.x, y: point.y).rotated(by: CGFloat(angle)).translatedBy(x: -point.x, y: -point.y)
+    }
 
     override func draw(_ rect: CGRect) {
+        drawFloor()
         drawSpokes()
         drawArcs()
+        
+//        let mask = CAShapeLayer()
+//        mask.fillRule = .evenOdd
+//        mask.path = wholeView.cgPath
+//        layer.mask = mask
+    }
+    
+    private func drawFloor() {
+        let circle = UIBezierPath(arcCenter: puzzleCenter,
+                                  radius: 0.48 * bounds.width,
+                                  startAngle: 0,
+                                  endAngle: 2 * CGFloat.pi,
+                                  clockwise: true)
+        PuzzleConst.floorColor.setFill()
+        circle.fill()
     }
     
     private func drawSpokes() {
+        let halfWidth = PuzzleConst.wallWidth / 2
         for spoke in spokes {
-            let innerRadius = ringRadiusFactors[spoke.innerRing] * bounds.width / 2
-            let outerRadius = ringRadiusFactors[spoke.outerRing] * bounds.width / 2
-            let innerEnd = CGPoint(x: puzzleCenter.x + innerRadius * cos(spoke.angle),
-                                   y: puzzleCenter.y + innerRadius * sin(spoke.angle))
-            let outerEnd = CGPoint(x: puzzleCenter.x + outerRadius * cos(spoke.angle),
-                                   y: puzzleCenter.y + outerRadius * sin(spoke.angle))
-            let line = UIBezierPath()
-            line.move(to: innerEnd)
-            line.addLine(to: outerEnd)
-            line.lineWidth = PuzzleConst.wallWidth
-            PuzzleConst.wallColor.setStroke()
-            line.stroke()
+            let innerRadius = ringRadiusFactors[spoke.innerRing] * bounds.width / 2 + halfWidth
+            let outerRadius = ringRadiusFactors[spoke.outerRing] * bounds.width / 2 - halfWidth
+            let rect = CGRect(x: puzzleCenter.x + innerRadius, y: puzzleCenter.y - halfWidth, width: outerRadius - innerRadius, height: PuzzleConst.wallWidth)
+            let shape = UIBezierPath(roundedRect: rect, cornerRadius: 0)
+            shape.apply(transformToRotate(angle: spoke.angle, about: puzzleCenter))
+            PuzzleConst.wallColor.setFill()
+            shape.fill()
+//            wholeView.append(shape)
         }
     }
     
     private func drawArcs() {
+        let halfWidth = PuzzleConst.wallWidth / 2
         for arc in arcs {
             let radius = ringRadiusFactors[arc.ring] * bounds.width / 2
-            let line = UIBezierPath(arcCenter: puzzleCenter,
-                                    radius: radius,
-                                    startAngle: arc.startAngle,
-                                    endAngle: arc.endAngle,
-                                    clockwise: true)
-            line.lineWidth = PuzzleConst.wallWidth
-            PuzzleConst.wallColor.setStroke()
-            line.stroke()
-            let startPoint = CGPoint(x: puzzleCenter.x + radius * cos(arc.startAngle),
-                                     y: puzzleCenter.y + radius * sin(arc.startAngle))
-            drawWallEndAtPoint(startPoint)
-            let endPoint = CGPoint(x: puzzleCenter.x + radius * cos(arc.endAngle),
-                                   y: puzzleCenter.y + radius * sin(arc.endAngle))
-            drawWallEndAtPoint(endPoint)
+            let shape = UIBezierPath()
+            // outer arc
+            shape.addArc(withCenter: puzzleCenter,
+                         radius: radius + halfWidth,
+                         startAngle: arc.startAngle,
+                         endAngle: arc.endAngle,
+                         clockwise: true)
+            // end cap
+            let endCenter = CGPoint(x: puzzleCenter.x + radius * cos(arc.endAngle),
+                                    y: puzzleCenter.y + radius * sin(arc.endAngle))
+            shape.addArc(withCenter: endCenter,
+                         radius: halfWidth,
+                         startAngle: arc.endAngle,
+                         endAngle: arc.endAngle + CGFloat.pi,
+                         clockwise: true)
+            // inner arc
+            shape.addArc(withCenter: puzzleCenter,
+                         radius: radius - halfWidth,
+                         startAngle: arc.endAngle,
+                         endAngle: arc.startAngle,
+                         clockwise: false)
+            // start cap
+            let startCenter = CGPoint(x: puzzleCenter.x + radius * cos(arc.startAngle),
+                                      y: puzzleCenter.y + radius * sin(arc.startAngle))
+            shape.addArc(withCenter: startCenter,
+                         radius: halfWidth,
+                         startAngle: arc.startAngle + CGFloat.pi,
+                         endAngle: arc.startAngle,
+                         clockwise: true)
+            PuzzleConst.wallColor.setFill()
+            shape.fill()
+//            wholeView.append(shape)
         }
-    }
-    
-    private func drawWallEndAtPoint(_ point: CGPoint) {
-        let circle = UIBezierPath(arcCenter: point,
-                                  radius: PuzzleConst.wallWidth / 2,
-                                  startAngle: 0,
-                                  endAngle: 2 * CGFloat.pi,
-                                  clockwise: true)
-        PuzzleConst.wallColor.setFill()
-        circle.fill()
     }
 }
